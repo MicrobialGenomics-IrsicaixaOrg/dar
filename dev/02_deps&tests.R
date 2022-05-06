04a0b7bb0a6030f2eb47dc1955a46c24a2b1e1bb
xec-cm <fcatala@irsicaixa.es>
2022-05-06 10:49:20 +0200

    closes #3

--- dev/02_deps&tests.R
+++ dev/02_deps&tests.R
@@ -9,3 +9,6 @@ usethis::use_tidy_eval()
 ## TESTS
 usethis::use_test("test-true")
 
+## Add internal datasets ----
+## If you have data in your package
+usethis::use_data_raw(name = "metaHIV_phy_data", open = FALSE)
