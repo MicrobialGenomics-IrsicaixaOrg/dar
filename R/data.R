82597f2be47c9a6901f14513c78af766074ea89d
xec-cm <fcatala@irsicaixa.es>
2022-05-06 11:38:44 +0200

    fixes #6

--- R/data.R
+++ R/data.R
@@ -6,5 +6,5 @@
 #' @format A phyloseq object with 451 taxas, 30 samples, 3 sample variables and 7
 #'   taxonomic ranks.
 #'
-#' @source \url{s3://fcatala-09142020-eu-west-1/cloud_test/SpeciesQuantification/Kraken2}
+#' @source s3://fcatala-09142020-eu-west-1/cloud_test/SpeciesQuantification/Kraken2
 "metaHIV_phy"
