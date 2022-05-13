7c37a8def0908f5f457f4d190121b5c9bb8d44e8
xec-cm <fcatala@irsicaixa.es>
2022-05-13 12:52:54 +0200

    Revert "provisional"
    This reverts commit ae2ca9bde970f0ee551993366f94e68f78bc3b3b.

--- R/dar-package.R
+++
@@ -1,7 +0,0 @@
-#' @description
-#' To learn more about dar, start with the vignettes:
-#' `browseVignettes(package = "dar")`
-#' @keywords internal
-#' @importClassesFrom phyloseq phyloseq
-"_PACKAGE"
-
