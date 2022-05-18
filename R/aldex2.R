242ea8fc5062c25b664e546d021a88302c241b4e
xec-cm <fcatala@irsicaixa.es>
2022-05-18 14:24:28 +0200

    closes #10

---
+++ R/aldex2.R
@@ -0,0 +1,139 @@
+#' ALDEx2 analysis
+#'
+#' A differential abundance analysis for the comparison of two or more conditions. For
+#' example, single-organism and meta-RNA-seq high-throughput sequencing assays, or of
+#' selected and unselected values from in-vitro sequence selections. Uses a
+#' Dirichlet-multinomial model to infer abundance from counts, that has been optimized for
+#' three or more experimental replicates. Infers sampling variation and calculates the
+#' expected false discovery rate given the biological and sampling variation using the
+#' Wilcox rank test or Welches t-test (aldex.ttest) or the glm and Kruskal Wallis tests
+#' (aldex.glm). Reports both P and fdr values calculated by the Benjamini Hochberg
+#' correction (Not supported in `dar` package).
+#'
+#' @details The run_aldex function is a wrapper that performs log-ratio transformation and
+#'   statistical testing in a single line of code. Specifically, this function: (a)
+#'   generates Monte Carlo samples of the Dirichlet distribution for each sample, (b)
+#'   converts each instance using a log-ratio transform, then (c) returns test results for
+#'   two sample (Welch's t, Wilcoxon) test. This function also estimates effect size for
+#'   two sample analyses.
+#'
+#' @param rec A recipe object. The step will be added to the sequence of operations for
+#'   this recipe.
+#' @param mc.samples The number of Monte Carlo instances to use to estimate the underlying
+#'   distributions; since we are estimating central tendencies, 128 is usually sufficient,
+#'   but larger numbers may be .
+#' @param denom An any variable (all, iqlr, zero, lvha, median, user) indicating features
+#'   to use as the denominator for the Geometric Mean calculation The default "all" uses
+#'   the geometric mean abundance of all features. Using "median" returns the median
+#'   abundance of all features. Using "iqlr" uses the features that are between the first
+#'   and third quartile of the variance of the clr values across all samples. Using "zero"
+#'   uses the non-zero features in each grop as the denominator. This approach is an
+#'   extreme case where there are many nonzero features in one condition but many zeros in
+#'   another. Using "lvha" uses features that have low variance (bottom quartile) and high
+#'   relative abundance (top quartile in every sample). It is also possible to supply a
+#'   vector of row indices to use as the denominator. Here, the experimentalist is
+#'   determining a-priori which rows are thought to be invariant. In the case of RNA-seq,
+#'   this could include ribosomal protein genes and and other house-keeping genes. This
+#'   should be used with caution because the offsets may be different in the original data
+#'   and in the data used by the function because features that are 0 in all samples are
+#'   removed by aldex.clr.
+#' @param id A character string that is unique to this step to identify it.
+#'
+#' @include recipe-class.R
+#' @family Diff taxa steps
+#' @aliases step_aldex
+#' @return An object of class `recipe`
+#' @export
+methods::setGeneric(
+  name = "step_aldex",
+  def = function(rec,
+                 mc.samples = 128,
+                 denom = "all",
+                 id = rand_id("aldex")) {
+    standardGeneric("step_aldex")
+  }
+)
+
+#' @rdname step_aldex
+#' @export
+methods::setMethod(
+  f = "step_aldex",
+  signature = c(rec = "recipe"),
+  definition = function(rec,
+                        mc.samples,
+                        denom,
+                        id) {
+
+    recipes_pkg_check(required_pkgs_aldex())
+
+    add_step(
+      rec,
+      step_aldex_new(
+        mc.samples = mc.samples,
+        denom = denom,
+        id = id
+      )
+    )
+
+  }
+)
+
+#' @rdname step_aldex
+#' @keywords internal
+step_aldex_new <-
+  function(out_cut,
+           mc.samples,
+           denom,
+           id) {
+    step(
+      subclass = "aldex",
+      mc.samples = mc.samples,
+      denom = denom,
+      id = id
+    )
+  }
+
+#' @noRd
+#' @keywords internal
+required_pkgs_aldex <- function(x, ...) { c("bioc::ALDEx2") }
+
+#' @rdname step_aldex
+#' @export
+run_aldex <- function(rec, mc.samples, denom) {
+
+  phy <- get_phy(rec)
+  vars <- get_var(rec)
+  tax_level <- get_tax(rec)
+
+  phy <- phyloseq::tax_glom(phy, taxrank = tax_level, NArm = F)
+  vars %>%
+    purrr::set_names() %>%
+    purrr::map(function(var) {
+      get_comparisons(var, phy, as_list = TRUE, n_cut = 1) %>%
+        purrr::map_dfr(function(comparison) {
+          sample_data <- dplyr::filter(sample_data(rec), !!dplyr::sym(var) %in% comparison)
+          otu_table <-
+            otu_table(rec) %>%
+            dplyr::select(taxa_id, dplyr::all_of(sample_data$sample_id)) %>%
+            data.frame(row.names = 1) %>%
+            as.matrix()
+
+          clr <- suppressMessages(
+            ALDEx2::aldex.clr(
+              reads = otu_table,
+              conds = sample_data %>% dplyr::pull(!!var) %>% as.character(),
+              denom = denom,
+              mc.samples = mc.samples
+            )
+          )
+
+          dplyr::bind_cols(
+            suppressMessages(ALDEx2::aldex.ttest(clr)),
+            suppressMessages(ALDEx2::aldex.effect(clr, CI = TRUE))
+          ) %>%
+            dplyr::mutate(comparison = stringr::str_c(comparison, collapse = "-"), var = var) %>%
+            tibble::as_tibble(rownames = "taxa_id") %>%
+            dplyr::left_join(tax_table(rec), by = "taxa_id")
+        })
+    })
+}
