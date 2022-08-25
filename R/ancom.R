366b844adac135a4716526387e9495fca5092228
xec-cm <fcatala@irsicaixa.es>
2022-08-25 09:40:35 +0200

    closes #47

--- R/ancom.R
+++ R/ancom.R
@@ -8,23 +8,32 @@
 #'
 #' @param rec A recipe object. The step will be added to the sequence of
 #'   operations for this recipe.
-#' @param out_cut Numerical fraction between 0 and 1. For each taxon,
-#'   observations with proportion of mixture distribution less than out_cut will
-#'   be detected as outlier zeros; while observations with proportion of mixture
-#'   distribution greater than 1 - out_cut will be detected as outlier values.
-#' @param zero_cut Numerical fraction between 0 and 1. Taxa with proportion of
-#'   zeroes greater than zero_cut are not included in the analysis.
-#' @param lib_cut Numeric. Samples with library size less than lib_cut are not
-#'   included in the analysis.
-#' @param neg_lb Logical. TRUE indicates a taxon would be classified as a
-#'   structural zero in the corresponding experimental group using its
-#'   asymptotic lower bound. More specifically, neg_lb = TRUE indicates you are
-#'   using both criteria stated in section 3.2 of ANCOM-II to detect structural
-#'   zeros; Otherwise, neg_lb = FALSE will only use the equation 1 in section
-#'   3.2 of ANCOM-II for declaring structural zeros.
-#' @param p_adj_method Character. Specifying the method to adjust p-values for
-#'   multiple comparisons. Default is “BH” (Benjamini-Hochberg procedure).
-#' @param alpha Level of significance. Default is 0.05.
+#' @param formula the character string expresses how the microbial absolute
+#'   abundances for each taxon depend on the variables in metadata.
+#' @param p_adj_method method to adjust p-values by. Default is "holm". Options
+#'   include "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr",
+#'   "none". See p.adjust for more details.
+#' @param zero_cut a numerical fraction between 0 and 1. Taxa with proportion of
+#'   zeroes greater than zero_cut will be excluded in the analysis. Default is
+#'   0.90.
+#' @param lib_cut a numerical threshold for filtering samples based on library
+#'   sizes. Samples with library sizes less than lib_cut will be excluded in the
+#'   analysis. Default is 0, i.e. do not filter any sample.
+#' @param group the name of the group variable in metadata. Specifying group is
+#'   required for detecting structural zeros and performing global test.
+#' @param struc_zero whether to detect structural zeros. Default is FALSE.
+#' @param neg_lb whether to classify a taxon as a structural zero in the
+#'   corresponding study group using its asymptotic lower bound. Default is
+#'   FALSE.
+#' @param tol the iteration convergence tolerance for the E-M algorithm. Default
+#'   is 1e-05.
+#' @param max_iter the maximum number of iterations for the E-M algorithm.
+#'   Default is 100.
+#' @param conserve whether to use a conservative variance estimate of the test
+#'   statistic. It is recommended if the sample size is small and/or the number
+#'   of differentially abundant taxa is believed to be large. Default is FALSE.
+#' @param alpha level of significance. Default is 0.05.
+#' @param global whether to perform global test. Default is FALSE.
 #' @param rarefy Boolean indicating if OTU counts must be rarefyed. This
 #'   rarefaction uses the standard R sample function to resample from the
 #'   abundance values in the otu_table component of the first argument, physeq.
@@ -39,26 +48,32 @@
 #' @aliases step_ancom
 #' @return An object of class `recipe`
 #' @export
-#' @examples 
+#' @examples
 #' data(metaHIV_phy)
-#' 
+#'
 #' ## Init recipe
 #' rec <- recipe(metaHIV_phy, "RiskGroup2", "Species")
 #' rec
-#' 
+#'
 #' ## Define ANCOM step with default parameters
 #' rec <- step_ancom(rec)
 #' rec
 methods::setGeneric(
   name = "step_ancom",
   def = function(rec,
-                 out_cut = 0.05,
+                 formula = get_var(rec),
+                 p_adj_method = "holm",
                  zero_cut = 0.9,
                  lib_cut = 0,
-                 neg_lb = FALSE,
-                 p_adj_method = "BH",
-                 alpha = 0.05,
-                 rarefy = FALSE,
+                 group = NULL,
+                 struc_zero = FALSE,
+                 neg_lb	= FALSE,
+                 tol = 1e-05, 
+                 max_iter = 100, 
+                 conserve = FALSE, 
+                 alpha = 0.05, 
+                 global = FALSE, 
+                 rarefy = FALSE, 
                  id = rand_id("ancom")) {
     standardGeneric("step_ancom")
   }
@@ -70,26 +85,38 @@ methods::setMethod(
   f = "step_ancom",
   signature = c(rec = "recipe"),
   definition = function(rec,
-                        out_cut,
+                        formula,
+                        p_adj_method,
                         zero_cut,
                         lib_cut,
+                        group,
+                        struc_zero,
                         neg_lb,
-                        p_adj_method,
-                        alpha,
-                        rarefy,
+                        tol, 
+                        max_iter, 
+                        conserve, 
+                        alpha, 
+                        global, 
+                        rarefy, 
                         id) {
 
     recipes_pkg_check(required_pkgs_ancom(), "step_ancom()")
     add_step(
       rec,
       step_ancom_new(
-        out_cut = out_cut,
+        formula = formula,
+        p_adj_method = p_adj_method,
         zero_cut = zero_cut,
         lib_cut = lib_cut,
+        group = group,
+        struc_zero = struc_zero,
         neg_lb = neg_lb,
-        p_adj_method = p_adj_method,
-        alpha = alpha,
-        rarefy = rarefy,
+        tol = tol, 
+        max_iter = max_iter, 
+        conserve = conserve, 
+        alpha = alpha, 
+        global = global, 
+        rarefy = rarefy, 
         id = id
       )
     )
@@ -99,100 +126,108 @@ methods::setMethod(
 #' @noRd
 #' @keywords internal
 step_ancom_new <-
-  function(out_cut,
+  function(formula,
+           p_adj_method,
            zero_cut,
            lib_cut,
+           group,
+           struc_zero,
            neg_lb,
-           p_adj_method,
-           alpha,
-           rarefy,
+           tol, 
+           max_iter, 
+           conserve, 
+           alpha, 
+           global, 
+           rarefy, 
            id) {
     step(
       subclass = "ancom",
-      out_cut = out_cut,
+      formula = formula,
+      p_adj_method = p_adj_method,
       zero_cut = zero_cut,
       lib_cut = lib_cut,
+      group = group,
+      struc_zero = struc_zero,
       neg_lb = neg_lb,
-      p_adj_method = p_adj_method,
-      alpha = alpha,
-      rarefy = rarefy,
+      tol = tol, 
+      max_iter = max_iter, 
+      conserve = conserve, 
+      alpha = alpha, 
+      global = global, 
+      rarefy = rarefy, 
       id = id
     )
   }
 
 #' @noRd
 #' @keywords internal
-required_pkgs_ancom <- function(x, ...) { c("xec-cm/ANCOM") }
+required_pkgs_ancom <- function(x, ...) { c("bioc::ANCOBC") }
 
 #' @noRd
 #' @keywords internal
 run_ancom <- function(rec,
-                      out_cut,
+                      formula,
+                      p_adj_method,
                       zero_cut,
                       lib_cut,
+                      group,
+                      struc_zero,
                       neg_lb,
-                      p_adj_method,
-                      alpha,
-                      rarefy) {
-    
+                      tol, 
+                      max_iter, 
+                      conserve, 
+                      alpha, 
+                      global, 
+                      rarefy, 
+                      id) {
+  
   phy <- get_phy(rec)
   vars <- get_var(rec)
   tax_level <- get_tax(rec)
-  if (rarefy) { 
-    phy <- phyloseq::rarefy_even_depth(phy, rngseed = 1234, verbose = FALSE) 
+  if (rarefy) {
+    phy <- phyloseq::rarefy_even_depth(phy, rngseed = 1234, verbose = FALSE)
   }
+  
   phy <- phyloseq::tax_glom(phy, taxrank = tax_level, NArm = FALSE)
-
   vars %>%
     purrr::set_names() %>%
     purrr::map(function(var) {
       get_comparisons(var, phy, as_list = TRUE, n_cut = 1) %>%
         purrr::map_dfr(function(comparison) {
-          sample_data <- 
-            dplyr::filter(sample_data(rec), !!dplyr::sym(var) %in% comparison)
+          vct_comp <- glue::glue("c('{comparison[1]}', '{comparison[2]}')")
+          s_phy <-
+            glue::glue("phyloseq::subset_samples(phy, {var} %in% {vct_comp})") %>%
+            parse(text = .) %>%
+            eval()
           
-          otu_table <-
-            otu_table(rec) %>%
-            dplyr::select(taxa_id, dplyr::all_of(sample_data$sample_id)) %>%
-            data.frame(row.names = 1) %>%
-            as.matrix()
-
-          prepro <- ANCOM::feature_table_pre_process(
-            feature_table = otu_table,
-            meta_data = sample_data,
-            sample_var = 'sample_id',
-            lib_cut = lib_cut,
-            out_cut = out_cut,
-            zero_cut = zero_cut
-          )
-
-          ancom_res <- ANCOM::ANCOM(
-            feature_table = prepro$feature_table,
-            meta_data = prepro$meta_data,
-            struc_zero = prepro$structure_zeros,
-            main_var = var,
+          res <- ANCOMBC::ancombc(
+            phyloseq = s_phy,
+            formula = var,
             p_adj_method = p_adj_method,
-            alpha = alpha
+            zero_cut = zero_cut,
+            lib_cut = lib_cut,
+            group = group,
+            struc_zero = struc_zero,
+            neg_lb = neg_lb,
+            tol = tol,
+            max_iter = max_iter,
+            conserve = conserve,
+            alpha = alpha,
+            global = global
           )
-
-          n_taxa <- ifelse(
-            is.null(prepro$struc_zero),
-            nrow(prepro$feature_table),
-            sum(apply(prepro$struc_zero, 1, sum) == 0)
-          )
-
-          ancom_res$out %>%
-            tibble::as_tibble() %>%
+          
+          res$res %>%
+            purrr::map2_dfc(names(res$res), ~ {
+              tibble::tibble(!!.y := .x[[1]])
+            }) %>%
             dplyr::mutate(
-              clr_mean_diff = ancom_res$fig$data$x,
-              structural_zero = ancom_res$fig$data$zero_ind,
-              x_label = min(ancom_res$fig$data$x),
-              y_label = 0.7 * (n_taxa - 1),
+              taxa_id = rownames(res$res$beta),
+              .before = 1,
               comparison = stringr::str_c(comparison, collapse = "_"),
               var = var
             ) %>%
             dplyr::left_join(tax_table(rec), by = "taxa_id") %>%
-            dplyr::filter(.data$detected_0.7 == TRUE)
+            dplyr::filter(.data$diff_abn == TRUE)
         })
-    })
+    }) 
 }
